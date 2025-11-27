2627516f6c6a66c5b509ea3324a1048974998b02
Basyir <basyirzainuddin@gmail.com>
2025-11-27 22:02:08 +0800

    work on marquee animation

--- src/components/animated-scroll/AnimatedScroll.jsx
+++ src/components/animated-scroll/AnimatedScroll.jsx
@@ -1,10 +1,17 @@
 import TextSpan from "./TextSpan";
 import "../../index.css"
 
+function randomSort(arr) {
+  	return arr
+    	.map((val) => ({ val, sort: Math.random() }))
+    	.sort((a, b) => a.sort - b.sort)
+    	.map(({ val }) => val);
+}
+
 export default function AnimatedScroll({ stuffs }) {
 
     const spans = stuffs.map(stuff =>
-        <TextSpan key={stuff.id} text={stuff.text} />
+        <TextSpan key={stuff.id} text={stuff.name} />
     );
 
     return (
@@ -12,18 +19,27 @@ export default function AnimatedScroll({ stuffs }) {
             <div className="relative flex overflow-hidden">
                 <div className="whitespace-nowrap animate-scroll">
                     {spans}
+                </div>
+                <div className="whitespace-nowrap animate-scroll2">
+                    <div className="flex">{spans}</div>
+                </div>
+            </div>
+            <div className="relative flex overflow-hidden">
+                <div className="whitespace-nowrap animate-scroll">
                     {spans}
                 </div>
-                {/* <div className="whitespace-nowrap animate-scroll2">
+                <div className="whitespace-nowrap animate-scroll2">
                     <div className="flex">{spans}</div>
-                </div> */}
+                </div>
             </div>
-            {/* <div className="">
-                {spans}
+            <div className="relative flex overflow-hidden">
+                <div className="whitespace-nowrap animate-scroll">
+                    {spans}
+                </div>
+                <div className="whitespace-nowrap animate-scroll2">
+                    <div className="flex">{spans}</div>
+                </div>
             </div>
-            <div className="flex ">
-                {spans}
-            </div> */}
         </>
     )
 }
--- src/components/animated-scroll/TextSpan.jsx
+++ src/components/animated-scroll/TextSpan.jsx
@@ -3,7 +3,12 @@ import '../../index.css'
 export default function TextSpan( {key, text} ) {
 
     return (
-        <span className="">
+        <span className=
+        "inline-flex m-[10px] tracking-[0.1em] \
+        bg-white text-[#030000] px-[20px] py-[15px] \
+        rounded-[25px] border border-white transition \
+        duration-500 shadow-[-3px_3px_2px_rgba(0,0,0,0.3)]"
+        >
             {text}
         </span>
     )
