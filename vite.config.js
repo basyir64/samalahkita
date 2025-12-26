b9098bc76d0e6f12098e0df6e8dac342ab3819a9
Basyir <basyirzainuddin@gmail.com>
2025-12-26 17:28:30 +0800

    changed base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/samalahkita/",
+  base: "/",
   plugins: [
     react({
       babel: {
